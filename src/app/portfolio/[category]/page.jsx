1c8a338de9d030063a7bb5efbd271c79f9b66cd9
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-03-28 23:19:39 +0530

    made getData fn. in blog and portfolio

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -14,11 +14,10 @@ const getData = (cat) => {
 };
 
 const Category = async ({ params }) => {
-  const cat = params.category;
-  const data = getData(cat);
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
-      <h1 className={styles.catTitle}>{cat}</h1>
+      <h1 className={styles.catTitle}>{params.category}</h1>
 
       {data.map((item) => (
         <div className={styles.item} key={item.id}>
