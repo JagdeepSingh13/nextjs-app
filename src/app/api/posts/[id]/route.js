5034a309c62a318bfdef4bbe80806c11dbdf7257
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-03-29 23:46:41 +0530

    added apis for creating posts and fetching posts of  user

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -14,3 +14,16 @@ export const GET = async (request, { params }) => {
     return new NextResponse(error, { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  const { id } = await params;
+
+  try {
+    await connect();
+    await Post.findByIdAndDelete(id);
+
+    return new NextResponse("Post deleted", { status: 200 });
+  } catch (error) {
+    return new NextResponse(error, { status: 500 });
+  }
+};
