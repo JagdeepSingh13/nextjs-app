836cf30d86c3515c42388327806d3851e1483b5e
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-03-29 12:28:55 +0530

    metadata for home and blog pages

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -14,7 +14,8 @@ const getData = (cat) => {
 };
 
 const Category = async ({ params }) => {
-  const data = getData(params.category);
+  const data = await getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
