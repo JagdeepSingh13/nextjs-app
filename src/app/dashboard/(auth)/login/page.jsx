70da4bd2c0484587dd48d038bfe923d3d4484eed
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-03-29 23:14:53 +0530

    dashboard, login router and logout button in navbar

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -3,8 +3,21 @@
 import React from "react";
 import styles from "./page.module.css";
 import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Login = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (session.status === "authenticated") {
+    router?.push("/dashboard");
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -32,6 +45,10 @@ const Login = () => {
         <button className={styles.button}>Login</button>
       </form>
       <button onClick={() => signIn("google")}>Login with Google</button>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/register">
+        Create new account
+      </Link>
     </div>
   );
 };
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
+import styles from "./page.module.css";
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -31,16 +33,27 @@ const Dashboard = () => {
   const session = useSession();
   console.log(session);
 
+  const router = useRouter();
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(
     "https://jsonplaceholder.typicode.com/posts",
     fetcher
   );
-
   console.log(data);
 
-  return <div>Dashboard</div>;
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+  // cannot see dashboard without logging in
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  if (session.status === "authenticated") {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
